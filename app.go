992d7f504aef3c11ce39d706a8d6c967e98c9e99
Claudio Netto <nettinhorama@gmail.com>
2017-04-22 02:02:38 -0300

    Using the new way to get the content. (:

--- app.go
+++ app.go
@@ -2,86 +2,32 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
-    "net/http"
     "os"
-    "github.com/nettoclaudio/dontpad-cli/html/util"
+    "github.com/nettoclaudio/dontpad-cli/user_interface"
+    "github.com/nettoclaudio/dontpad-cli/dontpad"
 )
 
 func main() {
-    
-    if ! hasFolderArgument() {
-        showProgramUsage()
-        
-        return
-    }
-    
-    folder := os.Args[1]
-    
-    if ! isValidFolder(folder) {
-        fmt.Fprintf(os.Stderr, "ERROR: That is not a valid folder.\n")
-        
-        return
-    }
-    
-    finalURL := formatDontpadURLWithFolder(folder)
-    
-    content, err := getFolderMessage(finalURL)
-    
+    var setup user_interface.SetUp
+    var err   error
+
+    setup, err = user_interface.ProcessCommands()
+
+    showAndExitOnError(err)
+
+    var response dontpad.Response
+
+    response, err = dontpad.GetContentFolder(setup.RemoteFolder)
+
+    showAndExitOnError(err)
+
+    fmt.Fprintf(os.Stdout, "%s", response.Body)
+}
+
+func showAndExitOnError(err error) {
     if err != nil {
-        fmt.Fprintf(os.Stderr, "ERROR: Problem getting message.\n%s", err)
-    }
-    
-    fmt.Printf("%s", content)
-}
+        fmt.Fprintf(os.Stderr, "%s", err)
 
-func hasFolderArgument() bool {
-    
-    if len(os.Args) < 2 {
-        return false
+        os.Exit(1)
     }
-    
-    return true
-}
-
-func showProgramUsage() {
-    fmt.Fprintf(os.Stderr, "Usage: %s FOLDER\n", os.Args[0])
-}
-
-func isValidFolder(folder string) bool {
-    
-    if folder == "" || folder == "/" {
-        return false
-    }
-    
-    return true
-}
-
-func formatDontpadURLWithFolder(folder string) string {
-    return "http://dontpad.com/" + folder
-}
-
-func getFolderMessage(url string) (string, error) {
-    
-    response, err := http.Get(url)
-    
-    if err != nil {
-        return "", err
-    }
-    
-    defer response.Body.Close()
-    
-    page, err := ioutil.ReadAll(response.Body)
-    
-    if err != nil {
-        return "", err
-    }
-    
-    content, err := util.ExtractFolderMessage(string(page))
-    
-    if err != nil {
-        return "", err
-    }
-
-    return content, nil
 }
